07d97fda262e98627184920fe800eb6427463dca
cpu.governor <cpu.governor@gmail.com>
2024-06-28 02:01:40 +0000

    linker errors debugging

--- src/Graphics[x][y].h
+++ src/Graphics[x][y].h
@@ -1,2 +1,17 @@
-#include"window.c"
-#include<SDL2/SDL.h>
+#ifndef GRAPHICS_X_Y_H_INCLUDED
+#define GRAPHICS_X_Y_H_INCLUDED
+
+#include <SDL2/SDL.h>
+#include <stdbool.h>
+#include <stdio.h>
+
+// Global variables
+SDL_Window *win;
+SDL_Renderer *ren;
+ 
+
+// Function prototypes
+bool init(const char *title, int width, int height);
+void quit();
+
+#endif // GRAPHICS_X_Y_H_INCLUDED
--- src/window.c
+++ src/window.c
@@ -1,10 +1,12 @@
-// SimpleGL
+// 
+#include"Graphics[x][y].h"
 #include<SDL2/SDL.h>
 #include<stdbool.h>
 #include<stdio.h>
 //Global variables
 SDL_Window *win=NULL;
 SDL_Renderer *ren=NULL;
+
 bool init(const char *title,int width, int height){
 	SDL_Init(SDL_INIT_EVERYTHING);
 	win=SDL_CreateWindow(title,SDL_WINDOWPOS_UNDEFINED,SDL_WINDOWPOS_UNDEFINED,width,height,SDL_WINDOW_SHOWN);
@@ -16,9 +18,3 @@ void quit(){
 	SDL_DestroyRenderer(ren);
 	SDL_Quit();
 }
-/*
-int main (){
-	puts("include SimpleGL.h in your project");
-	return 0;
-}
-*/
