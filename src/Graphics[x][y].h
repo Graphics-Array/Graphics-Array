c828e4c3b1253137c85c1501eb21e240975c98b0
CPU Governor <cpu.governor.com>
2024-07-02 23:30:43 +0100

    linker errors fixed

--- src/Graphics[x][y].h
+++ src/Graphics[x][y].h
@@ -1,17 +1,13 @@
-#ifndef GRAPHICS_X_Y_H_INCLUDED
-#define GRAPHICS_X_Y_H_INCLUDED
+#ifndef GRAPHICS_X_Y_H
+#define GRAPHICS_X_Y_H
 
 #include <SDL2/SDL.h>
 #include <stdbool.h>
-#include <stdio.h>
 
-// Global variables
-SDL_Window *win;
-SDL_Renderer *ren;
- 
+extern SDL_Window *win;
+extern SDL_Renderer *ren;
 
-// Function prototypes
 bool init(const char *title, int width, int height);
 void quit();
 
-#endif // GRAPHICS_X_Y_H_INCLUDED
+#endif
--- src/window.c
+++ src/window.c
@@ -1,20 +1,39 @@
-// 
-#include"Graphics[x][y].h"
-#include<SDL2/SDL.h>
-#include<stdbool.h>
-#include<stdio.h>
-//Global variables
-SDL_Window *win=NULL;
-SDL_Renderer *ren=NULL;
+#include "Graphics[x][y].h"
+#include <SDL2/SDL.h>
+#include <stdbool.h>
+#include <stdio.h>
 
-bool init(const char *title,int width, int height){
-	SDL_Init(SDL_INIT_EVERYTHING);
-	win=SDL_CreateWindow(title,SDL_WINDOWPOS_UNDEFINED,SDL_WINDOWPOS_UNDEFINED,width,height,SDL_WINDOW_SHOWN);
-	ren=SDL_CreateRenderer(win,-1,SDL_RENDERER_ACCELERATED);
-	return 0;
+// Global variables
+// SDL_Window *win = NULL;
+// SDL_Renderer *ren = NULL;
+
+bool init(const char *title, int width, int height) {
+    if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
+        printf("SDL_Init Error: %s\n", SDL_GetError());
+        return false;
+    }
+    win = SDL_CreateWindow(title, 100, 100, width, height, SDL_WINDOW_SHOWN);
+    if (win == NULL) {
+        printf("SDL_CreateWindow Error: %s\n", SDL_GetError());
+        SDL_Quit();
+        return false;
+    }
+    ren = SDL_CreateRenderer(win, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+    if (ren == NULL) {
+        SDL_DestroyWindow(win);
+        printf("SDL_CreateRenderer Error: %s\n", SDL_GetError());
+        SDL_Quit();
+        return false;
+    }
+    return true;
 }
-void quit(){
-	SDL_DestroyWindow(win);
-	SDL_DestroyRenderer(ren);
-	SDL_Quit();
+
+void quit() {
+    if (ren) {
+        SDL_DestroyRenderer(ren);
+    }
+    if (win) {
+        SDL_DestroyWindow(win);
+    }
+    SDL_Quit();
 }
